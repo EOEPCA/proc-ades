6570c689675f383817a52bf515d1c6626d9db113
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-08-05 18:48:03 +0200

    conf  wiki

--- openshift/003-build/003-build.sh
+++ openshift/003-build/003-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 NAMESPACE='eoepca'
-REPOSITORY="https://github.com/EOEPCA/proc-ades.git#feature/EOEPCA-145"
+REPOSITORY="https://github.com/EOEPCA/proc-ades#feature/EOEPCA-145"
 CONTEXT="3ty/proc-comm-zoo-1.2-alpha"
 NAME="proc-comm-zoo"
 DOCKERNAME="proc-comm-zoo:1.0"
@@ -11,3 +11,7 @@ oc --namespace=${NAMESPACE} new-build "${REPOSITORY}"  \
 --strategy=docker --context-dir=${CONTEXT} \
 --no-output=false  --to-docker=false --name="${NAME}" --output=name  --dry-run=false \
 --to=openshift/${DOCKERNAME}
+
+
+
+#oc --namespace=${NAMESPACE}   new-app "https://github.com/EOEPCA/proc-ades#feature/EOEPCA-145"
