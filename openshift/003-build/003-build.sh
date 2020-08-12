6aaa37c75b4cc440d925fbad84feadb4e539e78c
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-08-12 14:48:03 +0200

    tests fixed

--- openshift/003-build/003-build.sh
+++ openshift/003-build/003-build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 NAMESPACE='eoepca'
-REPOSITORY="https://github.com/EOEPCA/proc-ades#feature/EOEPCA-145"
+REPOSITORY="https://github.com/EOEPCA/proc-ades#no-terraform"
 CONTEXT="3ty/proc-comm-zoo-1.2-alpha"
 NAME="proc-comm-zoo"
 DOCKERNAME="proc-comm-zoo:1.0"
 
-#oc --namespace=${NAMESPACE} new-build "${REPOSITORY}"  \
-#--strategy=docker --context-dir=${CONTEXT} \
-#--no-output=false  --to-docker=false --name="${NAME}" --output=name  --dry-run=false \
-#--to=openshift/${DOCKERNAME}
-#
+oc --namespace=${NAMESPACE} new-build "${REPOSITORY}"  \
+--strategy=docker --context-dir=${CONTEXT} \
+--no-output=false  --to-docker=false --name="${NAME}" --output=name  --dry-run=false \
+--to=openshift/${DOCKERNAME}
 
 
-oc  --namespace=${NAMESPACE}  new-app httpd~https://github.com/EOEPCA/proc-ades#feature/EOEPCA-145
+
+#oc  --namespace=${NAMESPACE}  new-app 'httpd~https://github.com/EOEPCA/proc-ades#feature/no-terraform'
 
 #oc --namespace=${NAMESPACE}   new-app "https://github.com/EOEPCA/proc-ades#feature/EOEPCA-145"
